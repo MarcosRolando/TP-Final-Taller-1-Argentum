d5869242176bef541987cb3a04f9984b6fc79e4d
Agustin <aguscambiano@gmail.com>
2020-06-06 20:38:03 -0300

    Agrego FloorType

---
+++ Items/FloorType.h
@@ -0,0 +1,14 @@
+//
+// Created by agustin on 6/6/20.
+//
+
+#ifndef ARGENTUM_FLOORTYPE_H
+#define ARGENTUM_FLOORTYPE_H
+
+
+enum FloorType {
+    FLOOR_TYPE_GRASS, FLOOR_TYPE_SAND, FLOOR_TYPE_TREE
+};
+
+
+#endif //ARGENTUM_FLOORTYPE_H
