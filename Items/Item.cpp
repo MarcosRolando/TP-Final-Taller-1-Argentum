21e80ca7c60c874802051fee8985e3f606fb971a
Agustin <aguscambiano@gmail.com>
2020-06-06 17:41:18 -0300

    agrego clase Item

---
+++ Items/Item.cpp
@@ -0,0 +1,5 @@
+//
+// Created by agustin on 6/6/20.
+//
+
+#include "Item.h"
---
+++ Items/Item.h
@@ -0,0 +1,15 @@
+//
+// Created by agustin on 6/6/20.
+//
+
+#ifndef ARGENTUM_ITEM_H
+#define ARGENTUM_ITEM_H
+
+//Clase interfaz de la que heredan todos los items
+class Item {
+public:
+    virtual void use() = 0;
+};
+
+
+#endif //ARGENTUM_ITEM_H
