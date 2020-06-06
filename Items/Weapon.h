beee2d5c65e556035f54076754a091595c6f05da
Agustin <aguscambiano@gmail.com>
2020-06-06 18:12:23 -0300

    Creo weapon

---
+++ Items/Weapon.h
@@ -0,0 +1,17 @@
+//
+// Created by agustin on 6/6/20.
+//
+
+#ifndef ARGENTUM_WEAPON_H
+#define ARGENTUM_WEAPON_H
+
+
+#include "Item.h"
+
+class Weapon: public Item {
+private:
+    unsigned int damage;
+};
+
+
+#endif //ARGENTUM_WEAPON_H
