f45e0b48cd8e977f1ee461956412fe7c872c084b
Agustin <aguscambiano@gmail.com>
2020-06-06 20:42:09 -0300

    Agrego comentario

--- Items/FloorType.h
+++ Items/FloorType.h
@@ -5,7 +5,8 @@
 #ifndef ARGENTUM_FLOORTYPE_H
 #define ARGENTUM_FLOORTYPE_H
 
-
+//Este enum caracteriza el tipo de piso que esta en el tile, se usa para
+//comunicar al cliente el tile que va a tener que imprimir por pantalla
 enum FloorType {
     FLOOR_TYPE_GRASS, FLOOR_TYPE_SAND, FLOOR_TYPE_TREE
 };
