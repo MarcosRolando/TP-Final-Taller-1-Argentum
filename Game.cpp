419f73c9922c59ea6575bf357f941e15f0af54b5
Agustin <aguscambiano@gmail.com>
2020-06-07 18:18:09 -0300

    Agrego clase Game

---
+++ Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by agustin on 7/6/20.
+//
+
+#include "Game.h"
---
+++ Game.h
@@ -0,0 +1,14 @@
+//
+// Created by agustin on 7/6/20.
+//
+
+#ifndef ARGENTUM_GAME_H
+#define ARGENTUM_GAME_H
+
+
+class Game {
+
+};
+
+
+#endif //ARGENTUM_GAME_H
