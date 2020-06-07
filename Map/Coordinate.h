c928895a326532d2c5283d55973f16e0b454cf86
Agustin <aguscambiano@gmail.com>
2020-06-06 22:47:46 -0300

    Agrego Coordinate_t

---
+++ Map/Coordinate.h
@@ -0,0 +1,15 @@
+//
+// Created by agustin on 6/6/20.
+//
+
+#ifndef ARGENTUM_COORDINATE_H
+#define ARGENTUM_COORDINATE_H
+
+//Struct auxiliar para facilitar el paso de coordenadas, debido a esto no
+//tiene ningun comportamiento y sus atributos son publicos
+typedef struct Coordinate{
+    unsigned int position_i;
+    unsigned int position_j;
+}Coordinate_t;
+
+#endif //ARGENTUM_COORDINATE_H
