994465836ea13175ec8f669f20375869af30ec3e
Agustin <aguscambiano@gmail.com>
2020-06-09 18:18:04 -0300

    Muevo InventoryPlace a Item.h

--- Items/Item.h
+++ Items/Item.h
@@ -7,10 +7,19 @@
 
 #include "../Entities/Player.h"
 
+enum InventoryPLace{
+    INVENTORY_PLACE_NONE, INVENTORY_PLACE_HEAD, INVENTORY_PLACE_CHEST, INVENTORY_PLACE_WEAPON,
+    INVENTORY_PLACE_SHIELD
+};
+
+
 //Clase interfaz de la que heredan todos los items
+//Los items solo tienen sentido en un inventario de un jugador
 class Item {
 public:
-    virtual void use(Player& player) = 0;
+    //use debe retornar el lugar en el que debera equiparse el item una vez usado desde
+    //un inventario, si debe ser descartado entonces se tiene que retornar INVENTORY_PLACE_NONE
+    virtual InventoryPlace use(Player& player) = 0;
 };
 
 
