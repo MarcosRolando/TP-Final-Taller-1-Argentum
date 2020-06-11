90652636ba8ed61cf807e05baf21b3e0054c0324
Agustin <aguscambiano@gmail.com>
2020-06-10 23:16:33 -0300

    Agrego clase Shop

---
+++ Items/Shop.cpp
@@ -0,0 +1,5 @@
+//
+// Created by agustin on 10/6/20.
+//
+
+#include "Shop.h"
---
+++ Items/Shop.h
@@ -0,0 +1,14 @@
+//
+// Created by agustin on 10/6/20.
+//
+
+#ifndef ARGENTUM_SHOP_H
+#define ARGENTUM_SHOP_H
+
+
+class Shop {
+
+};
+
+
+#endif //ARGENTUM_SHOP_H
