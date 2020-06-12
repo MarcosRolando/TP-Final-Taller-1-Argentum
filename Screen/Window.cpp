978c1a85e03c730401cb3f073a4af8f61ddb53c3
MarcosRolando <marcosrolando.mr@gmail.com>
2020-06-12 17:15:44 -0300

    Arreglo que cuando pones fullscreen y habias achicado la pantalla la resolucion era una garcha

--- Screen/Window.cpp
+++ Screen/Window.cpp
@@ -98,6 +98,10 @@ void Window::handleEvent(SDL_Event& e) {
             SDL_SetWindowFullscreen(mWindow, SDL_FALSE);
             mFullScreen = false;
         } else {
+            mWidth = DEFAULT_SCREEN_WIDTH;
+            mHeight = DEFAULT_SCREEN_HEIGHT;
+            SDL_SetWindowSize(mWindow, mWidth, mHeight);
+            show();
             SDL_SetWindowFullscreen(mWindow, SDL_TRUE);
             mFullScreen = true;
             mMinimized = false;
