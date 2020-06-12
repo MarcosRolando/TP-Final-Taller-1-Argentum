25c23c4dacab798d0c8287e21d80030a97fa1e9b
Agustin <aguscambiano@gmail.com>
2020-06-12 20:09:12 -0300

    Creo archivos de calculator para copiar lo de Ivi

---
+++ Entities/Calculator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by agustin on 12/6/20.
+//
+
+#include "Calculator.h"
---
+++ Entities/Calculator.h
@@ -0,0 +1,14 @@
+//
+// Created by agustin on 12/6/20.
+//
+
+#ifndef ARGENTUM_CALCULATOR_H
+#define ARGENTUM_CALCULATOR_H
+
+
+class Calculator {
+
+};
+
+
+#endif //ARGENTUM_CALCULATOR_H
