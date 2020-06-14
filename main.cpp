fb39a0a8533dd041a2c559592c3a4ea0ac130075
MarcosRolando <marcosrolando.mr@gmail.com>
2020-06-14 15:00:40 -0300

    Mejoro la imagen del magic arrow

--- main.cpp
+++ main.cpp
@@ -61,7 +61,7 @@ int main(int argc, char* args[]) {
         Player player(repo, camera, 40, 30,pEquipment);
         NPC monster(repo, camera, 168, 30, Zombie);
 
-        Spell explosion(repo.getTexture(Heal), camera, TILE_WIDTH*3, TILE_HEIGHT*3);
+        Spell explosion(repo.getTexture(MagicMissil), camera, TILE_WIDTH*3, TILE_HEIGHT*3);
 
         Map map(repo, camera);
         Font font("../SDL/font.ttf", 25);
