b75c414c5602b888a089e89a41eae09e4815817e
Agustin <aguscambiano@gmail.com>
2020-06-16 20:03:11 -0300

    .

--- Entities/Citizens/Shop.h
+++ Entities/Citizens/Shop.h
@@ -18,20 +18,21 @@ public:
     Shop(std::unordered_map<std::string,
             std::list<std::shared_ptr<Item>>>&& initialItems, float buyingMultiplier, float sellingMultiplier);
 
-    //Implementa el comportamiento realizado al pedirle una lista de los items
-    //que tiene en venta, por default no hace nada, debe ser reimplementada
-    //si la clase hija tiene objetos en venta para listar
-    unsigned int list(std::list<ProductData>& products) override;
+    //Almacena en la lista los items que tiene en venta, los precios
+    //almacenados para cada producto fueron modificados utilizando el
+    //multiplicador recibido en el constructor
+    unsigned int list(std::list<ProductData>& products);
 
-    //Implementa el comportamiento realizado al intentar el jugador comprar un item
-    //con el nombre pasado, por default no hace nada, debe ser reimplementada
-    //si la clase hija puede comprar y vender items
-    void buy(Player& player, const std::string& itemName) override;
+    //Funcion utilizada cuando el jugador quiere comprar un item, si el jugador no
+    //tiene oro suficiente o el item pedido no esta guardado entonces no hace nada,
+    //sino
+    void buy(Player& player, const std::string& itemName);
 
+    //CAMBIAR COMMENT
     //Implementa el comportamiento realizado al intentar el jugador vender un item que se
     //encuentra en la posicion pasada, por default no hace nada, debe ser reimplementada
     //si la clase hija puede comprar y vender items
-    void sell(Player& player, const std::string& itemName) override;
+    void sell(Player& player, const std::string& itemName);
 };
 
 
