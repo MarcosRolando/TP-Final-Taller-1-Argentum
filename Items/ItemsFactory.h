addc9b2723e7105dda4f60c405839642e9bab26f
Agustin <aguscambiano@gmail.com>
2020-06-16 17:41:04 -0300

    Agrego constructor de ItemsFactory

--- Items/ItemsFactory.h
+++ Items/ItemsFactory.h
@@ -9,10 +9,19 @@
 #include <memory>
 #include "Item.h"
 
-typedef void (*objectCreator) (std::shared_ptr<Item>&&);
+typedef void (*objectCreator) (std::shared_ptr<Item>&);
 
 class ItemsFactory {
+private:
     std::unordered_map<std::string, objectCreator> itemsCreators;
+private:
+
+public:
+    ItemsFactory();
+
+    //Guarda una instancia del item pedido en item, si el nombre del item pasado
+    //no existe entonces tira la exepcion out_of_range
+    void storeItemInstance(std::string itemName, std::shared_ptr<Item>& item);
 };
 
 
