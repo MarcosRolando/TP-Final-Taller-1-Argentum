47b796a0efecd56243e791347df29dd184aa9ebd
Agustin <aguscambiano@gmail.com>
2020-06-17 19:24:25 -0300

    Agrego IronHelmet

---
+++ Items/Defense/Head/IronHelmet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by agustin on 17/6/20.
+//
+
+#include "IronHelmet.h"
---
+++ Items/Defense/Head/IronHelmet.h
@@ -0,0 +1,14 @@
+//
+// Created by agustin on 17/6/20.
+//
+
+#ifndef ARGENTUM_IRONHELMET_H
+#define ARGENTUM_IRONHELMET_H
+
+
+class IronHelmet {
+
+};
+
+
+#endif //ARGENTUM_IRONHELMET_H
