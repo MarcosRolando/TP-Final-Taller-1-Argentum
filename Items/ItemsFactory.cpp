5c98494a5d22fd0f2d4026cf752c6bb285cdda45
Agustin <aguscambiano@gmail.com>
2020-06-18 16:33:03 -0300

    Agrego funciones en el map de ItemsFactory

--- Items/ItemsFactory.cpp
+++ Items/ItemsFactory.cpp
@@ -4,6 +4,38 @@
 
 #include "ItemsFactory.h"
 
+#include "../Config/Configuration.h"
+
+using namespace Config;
+
+///////////////////////////PRIVATE//////////////////////////////////
+
+ItemsFactory::ItemsFactory() {
+    const Configuration& config = Configuration::getInstance();
+
+    itemsCreators[config.configClothingData(BLUE_TUNIC).name] = storeBlueTunic;
+    itemsCreators[config.configClothingData(LEATHER_ARMOR).name] = storeLeatherArmor;
+    itemsCreators[config.configClothingData(PLATE_ARMOR).name] = storePlateArmor;
+
+    itemsCreators[config.configClothingData(HOOD).name] = storeHood;
+    itemsCreators[config.configClothingData(IRON_HELMET).name] = storeIronHelmet;
+    itemsCreators[config.configClothingData(MAGIC_HAT).name] = storeMagicHat;
+
+    itemsCreators[config.configClothingData(IRON_SHIELD).name] = storeIronShield;
+    itemsCreators[config.configClothingData(TURTLE_SHIELD).name] = storeTurtleShield;
+
+    itemsCreators[config.configWeaponData(ASH_ROD).name] = storeAshRod;
+    itemsCreators[config.configWeaponData(COMPOSITE_BOW).name] = storeCompositeBow;
+    itemsCreators[config.configWeaponData(ELVEN_FLUTE).name] = storeElvenFlute;
+    itemsCreators[config.configWeaponData(GNARLED_STAFF).name] = storeGnarledStaff;
+    itemsCreators[config.configWeaponData(LINKED_STAFF).name] = storeLinkedStaff;
+    itemsCreators[config.configWeaponData(LONGSWORD).name] = storeLongsword;
+    itemsCreators[config.configWeaponData(SIMPLE_BOW).name] = storeSimpleBow;
+    itemsCreators[config.configWeaponData(WARHAMMER).name] = storeWarhammer;
+}
+
+///////////////////////////PUBLIC//////////////////////////////////
+
 ItemsFactory &ItemsFactory::getInstance() {
     static ItemsFactory factory;
     return factory;
--- Items/ItemsFactory.h
+++ Items/ItemsFactory.h
@@ -18,28 +18,28 @@ private:
 private:
     ItemsFactory();
 
-    void storeBlueTunic(std::shared_ptr<Item>& item);
-    void storeLeatherArmor(std::shared_ptr<Item>& item);
-    void storePlateArmor(std::shared_ptr<Item>& item);
+    static void storeBlueTunic(std::shared_ptr<Item>& item);
+    static void storeLeatherArmor(std::shared_ptr<Item>& item);
+    static void storePlateArmor(std::shared_ptr<Item>& item);
 
-    void storeHood(std::shared_ptr<Item>& item);
-    void storeIronHelmet(std::shared_ptr<Item>& item);
-    void storeMagicHat(std::shared_ptr<Item>& item);
+    static void storeHood(std::shared_ptr<Item>& item);
+    static void storeIronHelmet(std::shared_ptr<Item>& item);
+    static void storeMagicHat(std::shared_ptr<Item>& item);
 
-    void storeIronShield(std::shared_ptr<Item>& item);
-    void storeTurtleShield(std::shared_ptr<Item>& item);
+    static void storeIronShield(std::shared_ptr<Item>& item);
+    static void storeTurtleShield(std::shared_ptr<Item>& item);
 
-    void storeAshRod(std::shared_ptr<Item>& item);
-    void storeCompositeBow(std::shared_ptr<Item>& item);
-    void storeElvenFlute(std::shared_ptr<Item>& item);
-    void storeGnarledStaff(std::shared_ptr<Item>& item);
-    void storeLinkedStaff(std::shared_ptr<Item>& item);
-    void storeLongsword(std::shared_ptr<Item>& item);
-    void storeSimpleBow(std::shared_ptr<Item>& item);
-    void storeWarhammer(std::shared_ptr<Item>& item);
+    static void storeAshRod(std::shared_ptr<Item>& item);
+    static void storeCompositeBow(std::shared_ptr<Item>& item);
+    static void storeElvenFlute(std::shared_ptr<Item>& item);
+    static void storeGnarledStaff(std::shared_ptr<Item>& item);
+    static void storeLinkedStaff(std::shared_ptr<Item>& item);
+    static void storeLongsword(std::shared_ptr<Item>& item);
+    static void storeSimpleBow(std::shared_ptr<Item>& item);
+    static void storeWarhammer(std::shared_ptr<Item>& item);
 
     //VER COMO SE TRATA EL CASO ESPECIAL DE GOLD
-    void storeGold(std::shared_ptr<Item>& item, unsigned int ammount);
+    static void storeGold(std::shared_ptr<Item>& item, unsigned int ammount);
 
 public:
 
