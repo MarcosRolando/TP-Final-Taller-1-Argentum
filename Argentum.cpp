7f3459a61ae385252e7db7a805ed5a1b5be2142c
Agustin <aguscambiano@gmail.com>
2020-06-20 19:51:42 -0300

    Agrego clase argentum

---
+++ Argentum.cpp
@@ -0,0 +1,5 @@
+//
+// Created by agustin on 20/6/20.
+//
+
+#include "Argentum.h"
---
+++ Argentum.h
@@ -0,0 +1,14 @@
+//
+// Created by agustin on 20/6/20.
+//
+
+#ifndef ARGENTUM_ARGENTUM_H
+#define ARGENTUM_ARGENTUM_H
+
+
+class Argentum {
+
+};
+
+
+#endif //ARGENTUM_ARGENTUM_H
