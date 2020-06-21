f0e7680267c7657d36f263d208bd670be9a017ed
Agustin <aguscambiano@gmail.com>
2020-06-20 23:04:25 -0300

    Termino de crear MonsterFactory

--- MonstersFactory.h
+++ MonstersFactory.h
@@ -9,24 +9,32 @@
 #include <list>
 #include <memory>
 #include "Config/GameEnums.h"
+#include "Map/Coordinate.h"
 
 class Monster;
+class Game;
+class Map;
 
-typedef void (*monsterCreator)(std::unique_ptr<Monster>& monster);
+typedef void (*monsterCreator)(Game& game, Map& map, Coordinate initialPosition,
+                               std::unique_ptr<Monster>& monster);
 
 class MonstersFactory {
 private:
     std::unordered_map<GameType::Monster, monsterCreator> monsterCreators;
     std::list<GameType::Monster> existingMonsters;
 private:
-    static void _storeSpider(std::unique_ptr<Monster>& monster);
-    static void _storeSkeleton(std::unique_ptr<Monster>& monster);
-    static void _storeZombie(std::unique_ptr<Monster>& monster);
-    static void _storeGoblin(std::unique_ptr<Monster>& monster);
+    static void _storeSpider(Game& game, Map& map, Coordinate initialPosition,
+                             std::unique_ptr<Monster>& monster);
+    static void _storeSkeleton(Game& game, Map& map, Coordinate initialPosition,
+                               std::unique_ptr<Monster>& monster);
+    static void _storeZombie(Game& game, Map& map, Coordinate initialPosition,
+                             std::unique_ptr<Monster>& monster);
+    static void _storeGoblin(Game& game, Map& map, Coordinate initialPosition,
+                             std::unique_ptr<Monster>& monster);
 
 public:
     MonstersFactory();
-    void storeRandomMonster(std::unique_ptr<Monster>& monster);
+    void storeRandomMonster(Game& game, Map& map, std::unique_ptr<Monster>& monster, Coordinate initialPosition);
 };
 
 
