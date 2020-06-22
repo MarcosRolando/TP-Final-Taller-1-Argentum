e1961d313966e1f461db891e07374dfd5a43e130
MarcosRolando <marcosrolando.mr@gmail.com>
2020-06-22 15:48:21 -0300

    Empezamos con pruebas

---
+++ Tests/tests.cpp
@@ -0,0 +1,12 @@
+//
+// Created by marcos on 22/6/20.
+//
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE("Sin Mapa Test") {
+    int x = 3;
+    int y = 4;
+    REQUIRE((x + y) == 7);
+}
