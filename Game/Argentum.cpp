f4ddd396a6066fb765c3829491b646f5a54cbb31
MarcosRolando <marcosrolando.mr@gmail.com>
2020-06-21 21:10:52 -0300

    .

--- Game/Argentum.cpp
+++ Game/Argentum.cpp
@@ -3,13 +3,17 @@
 //
 
 #include "Argentum.h"
-
+#include <chrono>
 #include <atomic>
+
+using namespace std::chrono;
 
 void Argentum::execute() {
     std::atomic<bool> keepRunning(true);
 
-
+    high_resolution_clock::time_point time1 = high_resolution_clock::now();
+    high_resolution_clock::time_point time2 = high_resolution_clock::now();
+    duration<double, std::milli> timeStep = time2 - time1;
     //ACA SE TIRA THREAD PARA RECIBIR LA Q QUE CIERRA EL SERVER
 
 
