b8ec6ef0461431c492e880965ef346c32afa348d
Agustin <aguscambiano@gmail.com>
2020-06-23 18:02:39 -0300

    Arreglo error de compilacion en las pruebas

--- Tests/ItemTests.cpp
+++ Tests/ItemTests.cpp
@@ -14,10 +14,10 @@
 
 bool ItemTests::testInitialValues() {
     std::string name = "Mi nombre es Item!";
-    unsigned int price = 100;
-    Item item(ITEM_TYPE_CLOTHING, name, price);
+    //unsigned int price = 100;
+    Item item(ITEM_TYPE_CLOTHING, name/*, price*/);
     bool status = (item.getName() == name);
-    status = status && (item.getPrice() == price);
+    //status = status && (item.getPrice() == price);
     status = status && (item.type == ITEM_TYPE_CLOTHING);
     return status;
 }
