872556d6805d52bdf114cd416f4c9073a516827d
Agustin <aguscambiano@gmail.com>
2020-06-23 23:55:34 -0300

    Agrego UseItem

---
+++ Game/Events/UseItem.cpp
@@ -0,0 +1,14 @@
+//
+// Created by agustin on 23/6/20.
+//
+
+#include "UseItem.h"
+#include "../../Entities/Player.h"
+
+UseItem::UseItem(Player &player, int _position): player(player) {
+    position = _position;
+}
+
+void UseItem::operator()() {
+    player.useItem(position);
+}
