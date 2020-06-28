47aea7c6aaa3d806786d6c30f548eaf7a3b405b5
Agustin <aguscambiano@gmail.com>
2020-06-28 15:14:35 -0300

    Agrego parte de si esta vivo o no del mensaje de player

--- Entities/PlayerStats.cpp
+++ Entities/PlayerStats.cpp
@@ -155,6 +155,6 @@ void PlayerStats::storeAllRelevantData(std::stringstream& buffer) const {
     msgpack::pack(buffer, xpData);
     msgpack::type::tuple<int32_t, int32_t> manaData(currentMana, maxMana);
     msgpack::pack(buffer, manaData);
-    msgpack::type::tuple<int32_t, int32_t> lifeData(currentLife, maxLife);
+    msgpack::type::tuple<int32_t, int32_t, bool> lifeData(currentLife, maxLife, !isDead());
     msgpack::pack(buffer, lifeData);
 }
