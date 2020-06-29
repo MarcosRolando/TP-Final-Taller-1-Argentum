47025b9d0252302e26c7225b0bd985ff6d2a076c
MarcosRolando <marcosrolando.mr@gmail.com>
2020-06-29 17:59:25 -0300

    Empezamos a ver lo de manejar el evento move

--- Client/ClientEventHandler.cpp
+++ Client/ClientEventHandler.cpp
@@ -11,109 +11,30 @@ void ClientEventHandler::run() {
     Window& window = game.getWindow();
 
     while (!quit) {
-        std::unique_ptr<SDL_Event> event = events.pop();
-        if (event) { /*Si terminamos cerramos la cola entonces devuelve nullptr*/
-            if (event->type == SDL_QUIT) {
+        std::unique_ptr<SDL_Event> e = events.pop();
+        if (e) { /*Si terminamos cerramos la cola entonces devuelve nullptr*/
+            if (e->type == SDL_QUIT) {
                 quit = true;
                 break;
             }
-            selector.handleEvent(*event, game.getPlayerInfo().getXPos(),
-                                game.getPlayerInfo().getYPos(), window);
-            minichat.handleEvent(*event, game.getWindow());
-            /*
-            if (e.type == SDL_KEYDOWN && e.key.repeat == 0) {
-                 switch (e.key.keysym.sym) {
-                     case SDLK_UP:
-                         a = "up";
-                         minichat.queueText(a);
-                         player.move(UP);
-                         break;
-                     case SDLK_DOWN:
-                         a = "down";
-                         minichat.queueText(a);
-                         player.move(DOWN);
-                         break;
-                     case SDLK_LEFT:
-                         a = "left";
-                         minichat.queueText(a);
-                         player.move(LEFT);
-                         break;
-                     case SDLK_RIGHT:
-                         a = "right";
-                         minichat.queueText(a);
-                         player.move(RIGHT);
-                         break;
-                     case SDLK_SPACE://Esto es para probar pausar la musica
-                         if (!soundPlayer.isMusicPlaying()) {
-                             soundPlayer.pauseMusic();
-                         } else {
-                             soundPlayer.playMusic();
-                         }
-                         break;
-                 }
-                 */
-             }
-         }
+            std::string a = " ";
+            selector.handleEvent(*e, game.getPlayerInfo().getXPos(),
+                                 game.getPlayerInfo().getYPos(), window);
+            minichat.handleEvent(*e, game.getWindow());
 
+            if (e->type == SDL_KEYDOWN && e->key.repeat == 0) {
+                switch (e->key.keysym.sym) {
+                    case SDLK_UP:
+                        break;
+                    case SDLK_DOWN:
+                        break;
+                    case SDLK_LEFT:
+                        break;
+                    case SDLK_RIGHT:
+                        break;
+                }
 
-
-
-     /*
-     SDL_Event e;
-     Window& window = game.getWindow();
-     Minichat& minichat = game.getMinichat();
-     Selector& selector = game.getSelector();
-     int xPlayer, yPlayer;
-     //While application is running
-     while (!quit) {
-         //Esto capaz lo cambiamos de lugar despues
-         xPlayer = game.getPlayerInfo().getXPos();
-         yPlayer = game.getPlayerInfo().getYPos();
-         //Handle events on queue
-         while (SDL_PollEvent(&e) != 0) {
-             //User requests quit
-             if (e.type == SDL_QUIT) {
-                 quit = true;
-             }
-             selector.handleEvent(e, xPlayer, yPlayer, window);
-             minichat.handleEvent(e, game.getWindow());
-             window.handleEvent(e);
-
-             //std::string a = " ";//Para probar lo del minichat
-
-            if (e.type == SDL_KEYDOWN && e.key.repeat == 0) {
-                 switch (e.key.keysym.sym) {
-                     case SDLK_UP:
-                         a = "up";
-                         minichat.queueText(a);
-                         player.move(UP);
-                         break;
-                     case SDLK_DOWN:
-                         a = "down";
-                         minichat.queueText(a);
-                         player.move(DOWN);
-                         break;
-                     case SDLK_LEFT:
-                         a = "left";
-                         minichat.queueText(a);
-                         player.move(LEFT);
-                         break;
-                     case SDLK_RIGHT:
-                         a = "right";
-                         minichat.queueText(a);
-                         player.move(RIGHT);
-                         break;
-                     case SDLK_SPACE://Esto es para probar pausar la musica
-                         if (!soundPlayer.isMusicPlaying()) {
-                             soundPlayer.pauseMusic();
-                         } else {
-                             soundPlayer.playMusic();
-                         }
-                         break;
-                 }
-             }
-
-         }
-     }
-      */
+            }
+        }
+    }
 }
