73809c0c0d396e2d893b85348a16ef20efe40123
MarcosRolando <marcosrolando.mr@gmail.com>
2020-06-30 00:25:25 -0300

    Paso la reaccion de los monstruos a segundos

--- Entities/Monster.cpp
+++ Entities/Monster.cpp
@@ -120,7 +120,7 @@ void Monster::_move() {
 Monster::Monster(Game &_game, Coordinate initialPosition,
                  GameType::Entity _type):
                  Entity(_type, initialPosition, "pepito"/*todo*/),
-                 timeBetweenActions(Configuration::getInstance().configMonsterStats(_type).reactionSpeed),
+                 timeBetweenActions(Configuration::getInstance().configMonsterStats(_type).reactionSpeed * 1000),
                  stats(_type), map(_game.getMap()), game(_game) {
     elapsedTime = 0;
     type = _type;
