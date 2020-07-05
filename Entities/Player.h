3bf11e2502aec46ec3551d09ff66d00f80fd01f5
Agustin <aguscambiano@gmail.com>
2020-07-05 20:50:01 -0300

    Agreco comment a una funcion

--- Entities/Player.h
+++ Entities/Player.h
@@ -102,6 +102,9 @@ public:
     //Desequipa el arma
     bool unequip();
 
+    //Retorna una instancia de un ItemData que guarda el tipo e id de item y la
+    //posicion en la que el item fue dejado, si no se encuentra un item en la posicion
+    //recibida entonces se almacena -1 en el id del item
     ItemData dropItem(unsigned int itemPosition);
 
     void operator>>(std::stringstream& buffer) const override;
