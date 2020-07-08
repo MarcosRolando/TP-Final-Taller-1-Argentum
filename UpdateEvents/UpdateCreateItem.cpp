b8f3d5346fd365fc10cd368ab3728008ddb50363
MarcosRolando <marcosrolando.mr@gmail.com>
2020-07-07 23:40:56 -0300

    agrego mensaje de error en item por las dudas

--- UpdateEvents/UpdateCreateItem.cpp
+++ UpdateEvents/UpdateCreateItem.cpp
@@ -10,22 +10,26 @@ UpdateCreateItem::UpdateCreateItem(GameType::ItemType _type, int32_t _item,
                                     Coordinate _position) {
     ProtocolEnumTranslator translator;
     position = _position;
-    switch (_type) {
-        case GameType::ITEM_TYPE_GOLD:
-            item = Gold;
-            break;
-        case GameType::ITEM_TYPE_WEAPON:
-            item = translator.getWeaponDropTexture(static_cast<GameType::Weapon>(_item));
-            break;
-        case GameType::ITEM_TYPE_CLOTHING:
-            item = translator.getClothingDropTexture(static_cast<GameType::Clothing>(_item));
-            break;
-        case GameType::ITEM_TYPE_POTION:
-            item = translator.getPotionTexture(static_cast<GameType::Potion>(_item));
-            break;
-        case GameType::ITEM_TYPE_NONE:
-            throw TPException("Intentaron crear un item que no existia");
-            break;
+    try {
+        switch (_type) {
+            case GameType::ITEM_TYPE_GOLD:
+                item = Gold;
+                break;
+            case GameType::ITEM_TYPE_WEAPON:
+                item = translator.getWeaponDropTexture(static_cast<GameType::Weapon>(_item));
+                break;
+            case GameType::ITEM_TYPE_CLOTHING:
+                item = translator.getClothingDropTexture(static_cast<GameType::Clothing>(_item));
+                break;
+            case GameType::ITEM_TYPE_POTION:
+                item = translator.getPotionTexture(static_cast<GameType::Potion>(_item));
+                break;
+            case GameType::ITEM_TYPE_NONE:
+                throw TPException("Intentaron crear un item que no existia");
+                break;
+        }
+    } catch (std::exception& e) {
+        std::cerr << e.what() << " prob intentaro crear un item que yo no tenia traducido" << std::endl;
     }
 }
 
