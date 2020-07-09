9a1d51baab34f01f086ce2266e9f2126481711f7
MarcosRolando <marcosrolando.mr@gmail.com>
2020-07-09 17:05:57 -0300

    Agrego CitizenData para recibir su nickname en el map info inicial

---
+++ Client/CitizenData.h
@@ -0,0 +1,16 @@
+//
+// Created by marcos on 9/7/20.
+//
+
+#ifndef ARGENTUM_CITIZENDATA_H
+#define ARGENTUM_CITIZENDATA_H
+
+#include "../Texture/TextureID.h"
+#include <string>
+
+struct CitizenData {
+    TextureID texture;
+    std::string nickname;
+};
+
+#endif //ARGENTUM_CITIZENDATA_H
