e5dcc77b384d1c279e41bd1937b23a56be952c5f
Agustin <aguscambiano@gmail.com>
2020-07-10 23:34:38 -0300

    Empiezo a implementar RestoreStats

---
+++ Game/Events/RestoreStats.cpp
@@ -0,0 +1,15 @@
+//
+// Created by agustin on 10/7/20.
+//
+
+#include "RestoreStats.h"
+
+#include "../../Entities/Player.h"
+
+RestoreStats::RestoreStats(Game &game, Player &player, Coordinate _target): game(game), player(player) {
+    target = _target;
+}
+
+void RestoreStats::operator()(ServerProtocol &serverProtocol) {
+    game
+}
