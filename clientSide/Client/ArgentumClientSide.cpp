78c69b389d7fdd8dd6a3073db7e27a2fd1538c14
Ivan Soriano <ivan.soriano@live.com.ar>
2020-07-13 18:29:43 -0300

    Me habia faltado sacar un arv

--- clientSide/Client/ArgentumClientSide.cpp
+++ clientSide/Client/ArgentumClientSide.cpp
@@ -5,7 +5,7 @@
 #define INVALID_ARGUMENTS_MESSAGE "Error: argumentos invalidos."
 #define ARGUMENT_AMOUNT 1
 
-int ArgentumClientSide::run(int argc, char** argv) {
+int ArgentumClientSide::run(int argc) {
     if (argc != ARGUMENT_AMOUNT) {
         std::cout << INVALID_ARGUMENTS_MESSAGE << std::endl;
         return EXIT_FAILURE;
