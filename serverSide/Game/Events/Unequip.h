23b655a07599b942b74b1c03b6ddb41a0f334a05
Agustin <aguscambiano@gmail.com>
2020-07-18 19:36:51 -0300

    Agrego comentarios en unequip

--- serverSide/Game/Events/Unequip.h
+++ serverSide/Game/Events/Unequip.h
@@ -10,6 +10,8 @@
 #include "../../Items/Defense/Clothing.h"
 #include "../../../libs/GameEnums.h"
 
+//Clase que se almacena en la cola de eventos cuando un player quiere desequiparse
+//un item
 class Unequip : public Event {
 private:
     Player& player;
@@ -17,6 +19,10 @@ private:
 
 public:
     Unequip(Player& player, GameType::EquipmentPlace equipment);
+
+    //Intenta desequipar el item del lugar recibido en el constructor, si logra
+    //hacerlo entonces le comunica a los clientes que ese item ya no se encuentra
+    //equipado, agregandolo al protocolo
     void operator()(ServerProtocol& protocol) override;
 };
 
