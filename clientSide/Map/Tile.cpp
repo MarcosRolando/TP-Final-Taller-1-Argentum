0c2993d66f51a08516ba5aad97caa99359942371
MarcosRolando <marcosrolando.mr@gmail.com>
2020-07-19 10:17:03 -0300

    agrego un chequeo de error

--- clientSide/Map/Tile.cpp
+++ clientSide/Map/Tile.cpp
@@ -32,6 +32,9 @@ void Tile::createItem(Texture& _itemTexture) {
 }
 
 void Tile::addEntity(std::shared_ptr<Entity>& _entity) {
+    if (!entity.expired()) {
+        std::cerr << "pisaste un entity capo" << std::endl; //todo volar este mensaje
+    }
     entity = _entity;
 }
 
